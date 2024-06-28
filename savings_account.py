0d03a4372663c458fae73cc2acc386e2491a7f33
Dhan5hg <danieludoh952@gmail.com>
2024-06-28 11:01:14 -0700

    First commit boiiiii

---
+++ savings_account.py
@@ -0,0 +1,14 @@
+from account import Account
+
+
+class SavingsAccount(Account):
+    def __init__(self):
+        Account.__init__(self)
+
+    def withdraw(self, amount):
+        if amount < 2000:
+                super().withdraw(amount)
+        else:
+            print("You can not withdraw above your limit")
+savingsAccount = SavingsAccount()
+savingsAccount.withdraw(2000)
